c9d9640b7b222ea5765fdec7919340dd86ccb6b1
罗童 <luotong@luotongdeMacBook-Pro.local>
2019-08-09 21:13:33 +0800

    上传build

---
+++ build/precache-manifest.b0bf0be06c20297e4bb4714f61910bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651d757fed26d8fa1b718e3526d6f0f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283e63044b3efafe0379",
+    "url": "/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "db16af890d3ea25b62d1",
+    "url": "/static/css/main.4a750d78.chunk.css"
+  },
+  {
+    "revision": "283e63044b3efafe0379",
+    "url": "/static/js/2.444e8582.chunk.js"
+  },
+  {
+    "revision": "db16af890d3ea25b62d1",
+    "url": "/static/js/main.43bfdd40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
